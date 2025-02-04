d190e1b72bb0cb7332c2f581e22499bc58ab83a5
dkahffk1 <160700917+dkahffk1@users.noreply.github.com>
2025-02-04 16:55:20 +0900

    day1

---
+++ java/src/day1/_01_HelloWorld.java
@@ -0,0 +1,34 @@
+package day1;
+
+public class _01_HelloWorld {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.print("Hello World\n");
+		System.out.print("안녕하세요!!\n");
+		System.out.println("git test");
+		System.out.println("사과\t배\t로엔지"); //탭
+	    System.out.println("apple\tpearpear\torange");
+		System.out.println("우리집 강자지는 \"멍멍\" 짖어요");
+		
+		System.out.println("apple");
+		System.out.println("banana");
+		System.out.println("orange");//문자열을 풀력하므로 큰따음표 사용
+		
+		System.out.println("");
+		System.out.println(5);
+		System.out.println("3.14");
+		System.out.println('A');
+		System.out.println("");
+		
+		System.out.println("");
+		System.out.println(3+5);
+		System.out.println(3.9+5.72);
+		System.out.println(3.9*5.72);
+		System.out.println(10/3);
+		//정수 나눗셈의 결과는 몫을 나타낸다.
+		System.out.println(7/3.0);
+		//피연산중 실수가 하나라도 이쓰면 결과값은 실수로 출력됨.
+	}
+
+}
